b9b957a7257746145c89c586db68c036e49ed421
hbray <hbray@f6r9s14.paris.42.school>
2026-06-01 14:32:32 +0200

    tout fonctionne

--- srcs/exec/setup.c
+++ srcs/exec/setup.c
@@ -6,7 +6,7 @@
 /*   By: hbray <hbray@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/20 09:38:28 by hbray             #+#    #+#             */
-/*   Updated: 2026/06/01 11:25:57 by mmusquer         ###   ########.fr       */
+/*   Updated: 2026/06/01 14:28:19 by hbray            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,23 @@
 
 void	init_win(t_cub *cub)
 {
-	cub->texture[0].img = mlx_xpm_file_to_image(cub->mlx,
-			"sprites/path_to_the_north_texture.xpm", &cub->texture[0].width,
-			&cub->texture[0].height);
+	cub->texture[0].img = mlx_xpm_file_to_image(cub->mlx, cub->para->no_t,
+			&cub->texture[0].width, &cub->texture[0].height);
 	cub->texture[0].data = mlx_get_data_addr(cub->texture[0].img,
 			&cub->texture[0].bpp, &cub->texture[0].size_line,
 			&cub->texture[0].endian);
-	cub->texture[1].img = mlx_xpm_file_to_image(cub->mlx,
-			"sprites/path_to_the_south_texture.xpm", &cub->texture[1].width,
-			&cub->texture[1].height);
+	cub->texture[1].img = mlx_xpm_file_to_image(cub->mlx, cub->para->so_t,
+			&cub->texture[1].width, &cub->texture[1].height);
 	cub->texture[1].data = mlx_get_data_addr(cub->texture[1].img,
 			&cub->texture[1].bpp, &cub->texture[1].size_line,
 			&cub->texture[1].endian);
-	cub->texture[2].img = mlx_xpm_file_to_image(cub->mlx,
-			"sprites/path_to_the_west_texture.xpm", &cub->texture[2].width,
-			&cub->texture[2].height);
+	cub->texture[2].img = mlx_xpm_file_to_image(cub->mlx, cub->para->we_t,
+			&cub->texture[2].width, &cub->texture[2].height);
 	cub->texture[2].data = mlx_get_data_addr(cub->texture[2].img,
 			&cub->texture[2].bpp, &cub->texture[2].size_line,
 			&cub->texture[2].endian);
-	cub->texture[3].img = mlx_xpm_file_to_image(cub->mlx,
-			"sprites/path_to_the_east_texture.xpm", &cub->texture[3].width,
-			&cub->texture[3].height);
+	cub->texture[3].img = mlx_xpm_file_to_image(cub->mlx, cub->para->ea_t,
+			&cub->texture[3].width, &cub->texture[3].height);
 	cub->texture[3].data = mlx_get_data_addr(cub->texture[3].img,
 			&cub->texture[3].bpp, &cub->texture[3].size_line,
 			&cub->texture[3].endian);
